7c621fe53bb88add782695e7d6d88c218584a466
Lolchii <61840728+Lolchii@users.noreply.github.com>
2021-05-05 18:01:31 -0400

    Resolvers and validators
    login and validate login info

--- models/User.js
+++ models/User.js
@@ -4,10 +4,19 @@ const userSchema = new Schema({
     username: String,
     password: String,
     email: String,
-    createdAt: String
-    // postCount: Integer,
-    // followerCount: Integer,
-    // followingCount: Integer
+    createdAt: String,
+    post: {
+        type: Schema.Types.ObjectId,
+        ref: 'posts'
+    },
+    follower: {
+        type: Schema.Types.ObjectId,
+        ref: 'followers'
+    },
+    following: {
+        type: Schema.Types.ObjectId,
+        ref: 'following'
+    }
 });
 
 module.exports = model('User', userSchema);
